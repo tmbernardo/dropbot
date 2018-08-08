f4c7e1772b4764f3edd00a17c51aa8a1f19aa420
Derek Dang <dqdang17@gmail.com>
2018-08-08 16:32:48 -0700

    intial functional bot

--- acrnm.py
+++ acrnm.py
@@ -1,17 +1,19 @@
 from flask import Flask, request
+from pymessenger.bot import Bot
 from lxml import html
 import requests
 import time
 
 seconds = 1
 products = []
-cur_products = []
 
 app = Flask(__name__)
 with open('access-token.txt', 'r') as access_file:
-  access = access_file.read().replace('\n', '')
+    access = access_file.read().replace('\n', '')
+with open('verify-token.txt', 'r') as verify_file:
+    verify = verify_file.read().replace('\n', '')
 ACCESS_TOKEN = access
-VERIFY_TOKEN = 'VERIFY_TOKEN'
+VERIFY_TOKEN = verify
 bot = Bot(ACCESS_TOKEN)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,6 +26,7 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
         output = request.get_json()
+        products = get_products()
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
@@ -31,11 +34,25 @@ def receive_message():
                     #Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
                     if message['message'].get('text'):
-                        send_message(recipient_id)
+                        send_message(recipient_id, products)
                     #if user sends us a GIF, photo,video, or any other non-text item
                     if message['message'].get('attachments'):
-                        send_message(recipient_id)
+                        send_message(recipient_id, product)
         return "Message Processed"
+
+def get_products():
+    page = requests.get('http://acrnm.com')
+    tree = html.fromstring(page.content)
+
+    # create a list of products:
+    cur_products = tree.xpath('//div[@class="name"]/text()')
+    return cur_products
+    # if(products != cur_products):
+    #     products = cur_products
+    #     # send notification to messenger
+
+    # # time.sleep(seconds)
+    # print(products)
 
 def verify_fb_token(token_sent):
     # take token sent by facebook and verify it matches the verify token you sent
@@ -44,7 +61,7 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-def send_message(recipient_id):
+def send_message(recipient_id, products):
     #sends user the text message provided via input response parameter
     response = ""
     for product in products:
@@ -55,16 +72,3 @@ def send_message(recipient_id):
 if __name__ == '__main__':
     app.run()
 
-while(True):
-    page = requests.get('http://acrnm.com')
-    tree = html.fromstring(page.content)
-
-    # create a list of products:
-    cur_products = tree.xpath('//div[@class="name"]/text()')
-
-    if(products != cur_products):
-        products = cur_products
-        # send notification to messenger
-
-    # time.sleep(seconds)
-    print(products)
