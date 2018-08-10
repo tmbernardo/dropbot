86c2ebbc53b9cc612ed7aeaa290d3229ce5bad6c
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-08-09 17:59:35 -0700

    db stuff

---
+++ connect.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+ 
+import psycopg2
+import dbhandler as db
+ 
+ 
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE users (
+            user_id SERIAL PRIMARY KEY,
+            fb_id VARCHAR(255) NOT NULL
+        )
+        """,
+        """ CREATE TABLE products (
+                prod_id SERIAL PRIMARY KEY,
+                prod_name VARCHAR(255) NOT NULL
+                )
+        """)
+    conn = None
+    try:
+        # read the connection parameters
+        params = db.config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+db.insert()
---
+++ dbhandler.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+from configparser import ConfigParser
+import psycopg2 
+ 
+def config(filename='db.ini', section='dropbot'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+ 
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+ 
+    return db
+
+def insert(table, column ,value):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO %s(%s)
+             VALUES(%s);"""
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (table, column, value,))
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def insert_list(table, column, valuelist):
+    """ insert multiple vendors into the vendors table  """
+    sql = "INSERT INTO %s(%s) VALUES(%s)"
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.executemany(sql,(table, column,valuelist,))
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def update_vendor(vendor_id, vendor_name):
+    """ update vendor name based on the vendor id """
+    sql = """ UPDATE vendors
+                SET vendor_name = %s
+                WHERE vendor_id = %s"""
+    conn = None
+    updated_rows = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute(sql, (vendor_name, vendor_id, ))
+        # get the number of updated rows
+        updated_rows = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return updated_rows
+
+def get_table(table):
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM %s", (table,))
+        row = cur.fetchone()
+ 
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+ 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def delete_part(table, ID):
+    """ delete part by part id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute("DELETE FROM %s WHERE vendor_id = %s", (table, ID,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return rows_deleted
