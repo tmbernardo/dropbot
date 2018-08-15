1726758e85fbc995331fdfda12743469e635a2f4
Derek Dang <dqdang17@gmail.com>
2018-08-15 16:34:10 -0700

    attachment=none

--- acrnm.py
+++ acrnm.py
@@ -103,7 +103,7 @@ def send_message(recipient_id, products):
     response = ""
     for product in products:
         response += product + '\n'
-    bot.send(recipient_id, response, metadata="DEVELOPER_DEFINED_METADATA")
+    bot.send(recipient_id, response, attachment=None, metadata="DEVELOPER_DEFINED_METADATA")
     return "success"
 
 if __name__ == '__main__':
