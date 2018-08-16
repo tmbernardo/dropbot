959560680ddbcf7c97c5160da178469f5cdb49aa
Derek Dang <dqdang17@gmail.com>
2018-08-15 18:22:35 -0700

    printing products

--- acrnm.py
+++ acrnm.py
@@ -98,8 +98,8 @@ def send_message(recipient_id, products):
     response = ""
     for product in products:
         response += product + '\n'
-        
-    bot.send(recipient_id, response, metadata="DEVELOPER_DEFINED_METADATA")
+    print(products)
+    bot.send(recipient_id, "response", metadata="DEVELOPER_DEFINED_METADATA")
     return "success"
 
 if __name__ == '__main__':
