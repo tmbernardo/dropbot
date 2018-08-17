05157d1d9a439c38bada34fa4785780f91e255a2
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-08-16 18:41:03 -0700

    adding log

--- messenger.py
+++ messenger.py
@@ -4,6 +4,7 @@ import dbhandler as db
 
 @page.handle_message
 def receive_message(event): 
+    print("receive_message")
     # get whatever message a user sent the bot
     page.send(event.sender_id, "\n".join(db.get_table("prod_name", "products")))
     return "Message Processed"
