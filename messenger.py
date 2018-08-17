8f03aa23c95c1f70099a7e7fd58096074eddb8bd
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-08-16 18:53:35 -0700

    change messenger

--- messenger.py
+++ messenger.py
@@ -1,9 +1,8 @@
-from fbmq import Page
 from server import page
 import dbhandler as db
 
 @page.handle_message
-def receive_message(event): 
+def message_handler(event): 
     print("receive_message")
     # get whatever message a user sent the bot
     page.send(event.sender_id, "\n".join(db.get_table("prod_name", "products")))
