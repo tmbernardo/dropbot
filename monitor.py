fadc3be8a616906b7e08ae3cedaa200d27e53451
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-08-17 17:39:42 -0700

    added notifications

--- monitor.py
+++ monitor.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
 from lxml import html
+from fbpage import page
 import requests
 import time
 import dbhandler as db
 import os
 
 seconds = 30
+
+def notify_all(cur_products):
+    for user in db.get_table("user_id", "users"):
+        page.send(user, "\n".join(cur_products))
+    print("All users notified")
+
 def get_current():
     while(True):
         page = requests.get('http://acrnm.com')
@@ -13,6 +20,8 @@ def get_current():
 
         # create a list of products:
         cur_products = tree.xpath('//div[@class="name"]/text()')
+        if cur_products != db.get_table("prod_id","products"):
+            notify_all()
         db.insert_list("products", "prod_name", cur_products)
         time.sleep(seconds)
 
