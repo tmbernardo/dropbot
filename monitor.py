1996f8d05f6085d219cddfb64993870d959a13b2
Derek Dang <dqdang17@gmail.com>
2018-08-21 17:58:29 -0700

    debugging get_current()

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ import time
 import requests
 import dbhandler as db
 
-seconds = 60
+seconds = 15
 
 def notify_all(diff):
     for user in db.get_table("users", "fb_id"):
@@ -22,6 +22,10 @@ def get_current():
         # create a list of products:
         cur_products = set(tree.xpath('//div[@class="name"]/text()'))
         old_prods = set(db.get_table("products","prod_name"))
+        print("old prods")
+        print(old_prods)
+        print("cur_products")
+        print(cur_products)
         if cur_products != old_prods:
             diff = list(cur_products.difference(old_prods))
             notify_all(diff)
