c4fe7be902d8da6ce76116b412c23f538e927d6e
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-08-23 15:42:37 -0700

    fixing db classes

--- dbhandler.py
+++ dbhandler.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python
 from sqlalchemy import Table, Column, Integer, BigInteger, String, ForeignKey, Sequence
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import joinedload, relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 import os
 import psycopg2
-import sqlalchemy
+import sqlalchemy as sql
 
 Base = declarative_base()
 
-url = os.environ['DATABASE_URL']
-#url="postgres://bjihtaefdfdzxn:5d519eff130605f764b523746a4b919e9f1b521c794a1c013f55375ce01a4463@ec2-50-16-196-138.compute-1.amazonaws.com:5432/d8k7t1e86358jk"
-engine = sqlalchemy.create_engine(url, pool_size=17, client_encoding='utf8')
+#url = os.environ['DATABASE_URL']
+url="postgres://bjihtaefdfdzxn:5d519eff130605f764b523746a4b919e9f1b521c794a1c013f55375ce01a4463@ec2-50-16-196-138.compute-1.amazonaws.com:5432/d8k7t1e86358jk"
+engine = sql.create_engine(url, pool_size=17, client_encoding='utf8')
 
-class users(Base):
-    __tablename__ = 'users'
-    user_id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+class Users(Base):
+    id = Column(Integer, primary_key=True)
     fb_id = Column(BigInteger, nullable=False, unique=True)
+    subscriptions = relationship('Products', secondary='Subs', backref=sql.orm.backref('subscribers', lazy='dynamic'))
 
-class products(Base):
-    __tablename__ = 'products'
-    prod_id = Column(Integer, Sequence('prod_id_seq'), primary_key=True)
+class Products(Base):
+    id = Column(Integer, primary_key=True)
     prod_name = Column(String, nullable=False, unique=True)
 
-class subscriptions(Base):
-    __tablename__ = 'subscriptions'
-    sub_id = Column(Integer, Sequence('sub_id_seq'), primary_key=True)
-    prod_id = Column(Integer, ForeignKey('products.prod_id'))
-    user_id = Column(Integer, ForeignKey('users.user_id'))
-    products = relationship("products", foreign_keys=[prod_id])
-    user = relationship("users", foreign_keys=[user_id])
+class Subs(Base):
+    prod_id = Column(Integer, ForeignKey('products.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
 
-class current(Base):
-    __tablename__ = 'current'
-    curr_id = Column(Integer, Sequence('curr_id_seq'), primary_key=True)
-    prod_id = Column(Integer, ForeignKey('products.prod_id'))
-    products = relationship("products", foreign_keys=[prod_id])
+class Current(Base):
+    id = Column(Integer, primary_key=True)
+    prod_id = Column(Integer, ForeignKey('products.id'))
 
 table_dict = {
         "users": users,
@@ -81,8 +74,19 @@ def get_join(table1, column1, table2, column2):
     sess.close()
     return results
 
+def get_current():
+    sess = start_sess()
+    query = sess.query(current).options(
+            joinedload(current.prod_id, innerjoin=True)\
+                    .joinedload(products.prod_id, innerjoin=True))
+    results = query.all()
+    sess.close()
+    return results
+
 def get_table(table, column):
     sess = start_sess()
     results = sess.query(getattr(table_dict[table],column)).all()
     sess.close()
     return list(zip(*results))[0]
+
+print(get_current())
