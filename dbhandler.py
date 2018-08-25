663b9319b84e0443476729d5c11d190524781065
Derek Dang <dqdang17@gmail.com>
2018-08-24 19:22:54 -0700

    uncomment database url

--- dbhandler.py
+++ dbhandler.py
@@ -10,8 +10,8 @@ import sqlalchemy as sql
 Base = declarative_base()
 
 #heroku pg:psql -a acrbot
-#url = os.environ['DATABASE_URL']
-url="postgres://bjihtaefdfdzxn:5d519eff130605f764b523746a4b919e9f1b521c794a1c013f55375ce01a4463@ec2-50-16-196-138.compute-1.amazonaws.com:5432/d8k7t1e86358jk"
+url = os.environ['DATABASE_URL']
+# url="postgres://bjihtaefdfdzxn:5d519eff130605f764b523746a4b919e9f1b521c794a1c013f55375ce01a4463@ec2-50-16-196-138.compute-1.amazonaws.com:5432/d8k7t1e86358jk"
 engine = sql.create_engine(url, pool_size=17, client_encoding='utf8')
 
 class Users(Base):
